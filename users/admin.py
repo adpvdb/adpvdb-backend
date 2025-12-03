cef41d7795d712121cef4f7814436f211fac6062
adPVDb <query.adpvdb@gmail.com>
2025-12-03 22:33:21 +0530

    setup users admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ("email", "name", "role", "is_staff", "is_active")
+    list_filter = ("role", "is_staff", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("name",)}),
+        ("Roles & Permissions", {"fields": ("role", "is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "role", "is_staff", "is_active")
+        }),
+    )
+
+    search_fields = ("email", "name")
+    ordering = ("email",)
+
+
+admin.site.register(User, UserAdmin)
+
