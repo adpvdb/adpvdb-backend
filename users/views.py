462adbd497680506bdce1ca36325dde72f108f10
adPVDb <query.adpvdb@gmail.com>
2025-12-03 21:26:03 +0530

    setup user models, serializers, views, urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import RegisterSerializer
+from .models import User
+
+class RegisterView(generic.CreateAPIView):
+    queryset = USer.objects.all()
+    serializer_class = RegisterSerializer
+
+
